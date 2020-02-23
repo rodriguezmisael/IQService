fa526b3063fa6f0eb07be0e45e29898e6246cba8
Misael Rodriguez <rodriguez.misael@gmail.com>
2020-02-23 18:39:05 -0400

    Initial commit

---
+++ Models/Customer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IQService.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public virtual CustomerType CustomerType { get; set; }
+        public bool IsActive { get; set; }
+        public virtual CustomerStatus CustomerStatus { get; set; }
+        public string PhysicalAddressLine1 { get; set; }
+        public string PhysicalAddressLine2 { get; set; }
+        public string PhysicalZipcode { get; set; }
+        public string MailAddressLine1 { get; set; }
+        public string MailAddressLine2 { get; set; }
+        public string MailZipcode { get; set; }
+        public string EmailAddress { get; set; }
+        public string Phone { get; set; }
+        public string MobilePhone { get; set; }
+        public string Fax { get; set; }
+
+    }
+
+}
---
+++ Models/CustomerType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IQService.Models
+{
+    public class CustomerType
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public virtual ICollection<Customer> Customers { get; set; }
+    }
+}
